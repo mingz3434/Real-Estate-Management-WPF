b1a2b267a3c4d8dd2cd2bcb654c8cd551849ab43
mingz3434 <mingz3434@gmail.com>
2023-11-15 19:03:54 +0800

    minor

--- Model/EstateTypes/ResiSeries/Apartment.cs
+++ Model/EstateTypes/ResiSeries/Apartment.cs
@@ -11,13 +11,10 @@ namespace Modern_Real_Estate.Model.EstateTypes
 {
     public class Apartment : Residential
     {
-        private string _subType;
-        public string SubType
+        public Apartment(int id, string streetName, int zipCode, string city, string country, double area, double price, double rate, string imagePath, int rooms) : base(id, streetName, zipCode, city, country, area, price, rate, imagePath, rooms)
         {
-            get { return "Apartment"; }
+            
         }
-
-
     }
 }
 
